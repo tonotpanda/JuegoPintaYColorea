d36c87af43c027c641d725ab91761eb48baa974b
tonotpanda <161296297+tonotpanda@users.noreply.github.com>
2024-11-18 15:57:59 +0100

    prova

--- app/src/main/java/com/example/juego/PaintView.kt
+++ app/src/main/java/com/example/juego/PaintView.kt
@@ -135,7 +135,6 @@ class PaintView(context: Context, attrs: AttributeSet?) : View(context, attrs) {
         }
     }
 
-    //hola
     override fun onTouchEvent(event: MotionEvent): Boolean {
         // Si las interacciones están bloqueadas, ignoramos los toques
         if (isInteractionBlocked) {
