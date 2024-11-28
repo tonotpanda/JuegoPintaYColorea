00c51949251445fef8c3076015f1e10f5c781102
AxelPolitecnics <aortegag2324@politecnics.barcelona>
2024-11-28 17:46:50 +0100

    porcentajes ajustados, voltear tables correcto, ocultar botones andorid correcto

--- app/src/main/java/com/example/juego/MainActivity.kt
+++ app/src/main/java/com/example/juego/MainActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -15,6 +16,7 @@ import kotlinx.coroutines.*
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import org.json.JSONArray
 import org.json.JSONObject
@@ -57,12 +59,13 @@ class MainActivity : AppCompatActivity() {
     private var juegoAcabado = false
     private lateinit var mediaPlayer: MediaPlayer
 
-    private val sounds = arrayOf(R.raw.abeja, R.raw.arcoiris, R.raw.caracol, R.raw.elefante, R.raw.gusano1,
-        R.raw.mono2, R.raw.luna, R.raw.medusa, R.raw.mono1, R.raw.gusano2, R.raw.mono3, R.raw.nube, R.raw.pez, R.raw.pulpo, R.raw.seta, R.raw.volcan)
+    private val sounds = arrayOf(
+        R.raw.abeja, R.raw.arcoiris, R.raw.caracol, R.raw.elefante, R.raw.gusano1,
+        R.raw.mono2, R.raw.luna, R.raw.medusa, R.raw.mono1, R.raw.gusano2,
+        R.raw.mono3, R.raw.nube, R.raw.pez, R.raw.pulpo, R.raw.seta, R.raw.volcan
+    )
 
-
-
-    private val runnable = object : Runnable {
+    private val tiempoRunnable = object : Runnable {
         override fun run() {
             tiempoEnSegundos++
             val minutos = tiempoEnSegundos / 60
@@ -75,15 +78,20 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
             handler.postDelayed(this, 1000)
-
         }
     }
 
+    // Handler y Runnable para ocultar los botones de navegación
+    private val hideHandler = Handler(Looper.getMainLooper())
+    private val hideNavigationRunnable = Runnable {
+        enableImmersiveMode()
+    }
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        enableImmersiveMode()
 
         characterType = intent.getStringExtra("character_type")
         imageName = intent.getStringExtra("character_name")
@@ -94,7 +102,6 @@ class MainActivity : AppCompatActivity() {
         btn_Salir = findViewById(R.id.Btn_salir)
         val recyclerView: RecyclerView = findViewById(R.id.recyclerView)
 
-        // Inicializa el adaptador de colores
         val paleta = Paleta()
         colorAdapter = ColorAdapter(this, paleta.getColorsForCurrentImage(0)) { selectedColor ->
             paintView.setColor(selectedColor)
@@ -104,20 +111,38 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = GridLayoutManager(this, 4)
         recyclerView.adapter = colorAdapter
 
-        // Cargar imágenes y sonidos en un hilo diferente
         loadResources()
+        handler.post(tiempoRunnable)
 
-        handler.post(runnable)
-
-        btn_Salir.setOnLongClickListener{
+        btn_Salir.setOnLongClickListener {
             juegoAcabado = true
             onImageCompleted()
             finish()
             true
         }
 
-        // Configura el PaintView para que llame a onImageCompleted cuando una imagen se complete
         paintView.setOnImageCompletedListener { onImageCompleted() }
+
+        // Detecta cuando los botones de navegación son visibles
+        window.decorView.setOnSystemUiVisibilityChangeListener { visibility ->
+            if ((visibility and View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                hideHandler.removeCallbacks(hideNavigationRunnable)
+                hideHandler.postDelayed(hideNavigationRunnable, 5000)
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        enableImmersiveMode()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        hideHandler.removeCallbacks(hideNavigationRunnable)
+        if (::mediaPlayer.isInitialized) {
+            mediaPlayer.release()
+        }
     }
 
     @SuppressLint("MissingSuperCall")
@@ -127,14 +152,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadResources() {
         CoroutineScope(Dispatchers.IO).launch {
-            // Pre-cargar imágenes y sonidos aquí
-            // Por ejemplo, cargar el primer drawable en un Bitmap
             for (index in img.indices) {
                 val (colorImg, _) = img[index]
-                resources.getDrawable(colorImg, null) // Pre-cargar el drawable
+                resources.getDrawable(colorImg, null)
             }
             withContext(Dispatchers.Main) {
-                loadImages(currentIndex) // Cargar la imagen inicial
+                loadImages(currentIndex)
             }
         }
     }
@@ -142,8 +165,8 @@ class MainActivity : AppCompatActivity() {
     private fun loadImages(index: Int) {
         if (index < img.size) {
             val (colorImg, _) = img[index]
-            imgColor.setImageResource(colorImg) // Mostrar imagen de color
-            updateColorPalette(index) // Actualizar paleta de colores
+            imgColor.setImageResource(colorImg)
+            updateColorPalette(index)
         }
     }
 
@@ -154,38 +177,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun playSound(soundResId: Int) {
-        // Liberar el MediaPlayer anterior si ya está inicializado
         if (::mediaPlayer.isInitialized) {
             mediaPlayer.release()
         }
-
         mediaPlayer = MediaPlayer.create(this, soundResId)
         mediaPlayer.start()
         mediaPlayer.setOnCompletionListener { it.release() }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        if (::mediaPlayer.isInitialized) {
-            mediaPlayer.release()
-        }
-    }
-
     private fun onImageCompleted() {
         colorAdapter.completeImage()
-
         currentIndex++
-        if (!juegoAcabado){
-        if (currentIndex < img.size) {
-            playSound(sounds[count])
-            count++
-            if (count >= sounds.size) {
-                count = 0
+        if (!juegoAcabado) {
+            if (currentIndex < img.size) {
+                playSound(sounds[count])
+                count++
+                if (count >= sounds.size) {
+                    count = 0
+                }
+            } else {
+                juegoAcabado = true
             }
-            juegoAcabado = false
-        }else{
-            juegoAcabado = true
-        }
         }
         if (!juegoAcabado) {
             dibujosHechos++
@@ -195,10 +207,7 @@ class MainActivity : AppCompatActivity() {
             val errores = paintView.getErrorCount()
             juntarEstadisticas(errores)
             Toast.makeText(this, "¡Juego terminado!", Toast.LENGTH_SHORT).show()
-
-            // Crear el Intent para pasar los datos
             val intent = Intent(this, PantallaFinalActivity::class.java)
-            // Pasa los datos al Intent
             intent.putExtra("character_type", characterType)
             intent.putExtra("character_name", imageName)
             intent.putExtra("errores", errores)
@@ -209,7 +218,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun juntarEstadisticas(errores: Int) {
-        // Asegurarte de que no sean nulos
         val personage = characterType ?: "Desconocido"
         val icono = imageName ?: "Desconocido"
         val dibujosCompletados = dibujosHechos
@@ -219,18 +227,14 @@ class MainActivity : AppCompatActivity() {
         añadirExploradorEnJson(this, alumno)
     }
 
-
     private fun añadirExploradorEnJson(context: Context, nuevoAlumno: Alumno) {
         val archivo = File(getExternalFilesDir(null), "Resultado_Alumnos.json")
         val jsonArray = if (archivo.exists()) {
-            // Leer el contenido existente y convertirlo a JSONArray
             JSONArray(archivo.readText())
         } else {
-            // Crear un nuevo JSONArray si el archivo no existe
             JSONArray()
         }
 
-        // Añadir el nuevo explorador
         jsonArray.put(JSONObject().apply {
             put("Personage", nuevoAlumno.personage)
             put("Icono", nuevoAlumno.icono)
@@ -239,13 +243,22 @@ class MainActivity : AppCompatActivity() {
             put("Errores", nuevoAlumno.errores)
         })
 
-        // Guardar el JSONArray actualizado en el archivo
         FileOutputStream(archivo).use { fos ->
             fos.write(jsonArray.toString().toByteArray())
         }
 
-        // Agrega un log para verificar que se ha creado el archivo
         Log.d("ArchivoJSON", "Archivo creado en: ${archivo.absolutePath}")
     }
 
+    @SuppressLint("ClickableViewAccessibility")
+    private fun enableImmersiveMode() {
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                )
+    }
 }
